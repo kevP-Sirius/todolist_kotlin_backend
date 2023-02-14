f3f80789cdba5cb64752972cb89b88ed7df2ed73
kevP-Sirius <kev972@gmail.com>
2023-02-15 00:42:03 +0100

    first commit

---
+++ controller/login/index.js
@@ -0,0 +1,48 @@
+const axios = require("axios");
+let loginInfo=async(req,res,ObjectId,db,moment,transporter,hashIt,axios,qs)=>{
+    try {
+        // { $set: { [`${columnName}`] : req.body.newData } };
+        moment.locale('fr')
+        const userExist = await new Promise(function (resolve, reject) {
+            db.collection('_user').find({
+                "_id": ObjectId(req.body.login),
+            }).toArray((err, user) => {
+                if (err) {
+                    return resolve(false)
+                }
+                if (user.length > 0) {
+                    return resolve(user)
+                }
+                if (user.length == 0) {
+                    return resolve(false)
+                }
+            })
+        });
+        if (!userExist) {
+            res.json({
+                error: "incorrect password or login"
+            })
+        }
+        if (userExist) {
+            const passwordMatch = await bcrypt.compare(req.body.password, userExist[0].password);
+            if (passwordMatch) {
+                const token = jwt.sign({
+                    id: userExist[0]._id,
+                })
+                res.json({
+                    token: token,
+                })
+            }
+            if (!passwordMatch) {
+                res.json({
+                    error: "incorrect password or login"
+                })
+            }
+        }
+
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+exports.loginInfoGenerator=loginInfo
---
+++ server.js
@@ -0,0 +1,57 @@
+var express = require('express');
+require('dotenv').config();
+var bodyParser = require('body-parser')
+var cors = require('cors')
+var app = express()
+app.use(bodyParser.urlencoded({ extended: true }));
+const bcrypt = require('bcrypt');
+var ObjectId = require('mongodb').ObjectId;
+const port = process.env.PORT || 9001;
+app.use(cors())
+var MongoClient = require('mongodb').MongoClient;
+var moment = require('moment');
+var momentDurationFormatSetup = require("moment-duration-format");
+var nodemailer = require('nodemailer');
+const axios = require('axios');
+const qs = require('qs');
+app.use(bodyParser.json());
+let dbUrl="mongodb://admin:eecbcdb9f950087b66a@localhost:27017/?authMechanism=DEFAULT"
+if(process.env.ENV=="dev"){
+  dbUrl="mongodb://admin:eecbcdb9f950087b66a@15.237.6.116:27017/?authMechanism=DEFAULT"
+}
+if(process.env.ENV=="prod"){
+  dbUrl="mongodb://admin:eecbcdb9f950087b66a@13.36.28.63:27017/?authMechanism=DEFAULT"
+}
+
+let hashIt = async(password)=>{
+  const salt = await bcrypt.genSalt(6);
+  const hashed = await bcrypt.hash(password, salt);
+  return hashed
+}
+
+transporter = nodemailer.createTransport({
+  port: 465,               // true for 465, false for other ports
+  host: "email-smtp.eu-west-3.amazonaws.com",
+      auth: {
+          user: 'AKIASZOYTYLKZDG4ZGDA',
+          pass: 'BMHySd2T1HirJBaL9sgvgtd46uKA72SCh/j1aIAApilk',
+          },
+  secure: true,
+  });
+
+MongoClient.connect(dbUrl, function(err, client) {
+  if (err) {
+    throw err;
+  }
+  db = client.db("expert_active");
+});
+app.listen(port, () => {
+  console.log(`mode : ${process.env.ENV}`)
+  console.log(`Example app listening on port ${port}`)
+})
+app.get('/login',async (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  let moduleGenerator = require('./controller/login/index.js');
+  await moduleGenerator.moduleRoute(req,res,ObjectId,db,moment,transporter,hashIt,axios,qs)
+})
+
